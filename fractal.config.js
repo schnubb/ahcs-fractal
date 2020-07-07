ea08b4c7c209e72147132d91a7cb83e090c08351
Petrakow, Andre (init) <andre.petrakow@init.de>
2020-07-07 20:29:20 +0200

    adds handlebars helpers

--- fractal.config.js
+++ fractal.config.js
@@ -1,8 +1,11 @@
 const { resolve } = require("path");
 const fractal = require("@frctl/fractal").create();
+const helpers = require("handlebars-helpers");
 
 fractal.set("project.title", "AHCS - Fractal Session");
 
-fractal.components.set("path",resolve(__dirname, "src/components"));
+fractal.components.set("path", resolve(__dirname, "src/components"));
+
+helpers({ handlebars: fractal.components.engine().handlebars });
 
 module.exports = fractal;
